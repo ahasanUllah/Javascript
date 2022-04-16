7becc20de553e9b20400d17ea5625943b8596882
ahasanUllah <88593825+ahasanUllah@users.noreply.github.com>
2022-04-17 03:01:06 +0600

    first

--- first.js
+++ first.js
@@ -1,5 +1,14 @@
-function myFunction(a, b){
-    console.log("hello");
-}
-let x = myFunction;
-console.log(x);
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(length);
+function hello() {}
+const car = {
+  name: "BMW",
+  model: 500,
+  color: "white",
+  start: function () {
+    console.log(this.color + " " + "is the color of this car");
+  },
+};
+console.log(car.name);
+car.start();
